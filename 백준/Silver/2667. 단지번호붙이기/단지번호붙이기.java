c90e735969d3e3c981dd5b079720661e6460c6df
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2023-01-24 10:59:12 +0900

    [Silver I] Title: 단지번호붙이기, Time: 148 ms, Memory: 14576 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,81 +1,71 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Main {
-	static boolean[][] visited;
-	static int[][] map;
-	static int cnt = 0;
-	static int[] addRow = { -1, 1, 0, 0 };
-	static int[] addCol = { 0, 0, -1, 1 };
-	static ArrayList<Integer> list = new ArrayList<Integer>();
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		map = new int[n][n];
-		for (int i = 0; i < map.length; i++) {
-			String[] temp = br.readLine().split("");
-			for (int j = 0; j < map.length; j++) {
-				map[i][j] = Integer.parseInt(temp[j]);
-			}
-		}
-		visited = new boolean[n][n];
-		for (int i = 0; i < map.length; i++) {
-			for (int j = 0; j < map.length; j++) {
-				if (map[i][j] == 1 && visited[i][j] == false) {
-					cnt = 1;
-					dfs(i,j);
-					//bfs(ni,nj);
-					list.add(cnt);
-				}
-			}
-		}
-		Collections.sort(list);
-		System.out.println(list.size());
-		for (Integer answer : list) {
-			System.out.println(answer);
-		}
-	}
-	
-	public static int dfs(int i, int j) {
-		visited[i][j] = true;
-		for (int k = 0; k < 4; k++) {
-			int ni = i + addRow[k];
-			int nj = j + addCol[k];
-			if (ni >= 0 && ni < map.length && nj >= 0 && nj < map.length) {
-				if (map[ni][nj] == 1 && visited[ni][nj] == false) {
-					cnt++;
-					dfs(ni,nj);
-				}
-			}
-		}
-		return cnt;
-	}
-	
-	public static int bfs(int i, int j) {
-		Queue<int[]> queue = new LinkedList<int[]>();
-		queue.add(new int[] {i,j});
-		visited[i][j] = true;
-		while (!queue.isEmpty()) {
-			int a = queue.poll()[0];
-			int b = queue.poll()[1];
-			for (int k = 0; k < 4; k++) {
-				int ni = a + addRow[k];
-				int nj = b + addCol[k];
-				if (ni >= 0 && ni < map.length && nj >= 0 && nj < map.length) {
-					if (map[ni][nj] == 1 && visited[ni][nj]) {
-						queue.add(new int[] {i,j});
-						visited[ni][nj] = true;
-						cnt++;
-					}
-				}
-			}
-		}
-		return cnt;
-	}
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static boolean[][] visited;
+    static int[] addRow = {0, 1, -1, 0};
+    static int[] addCol = {1, 0, 0, -1};
+    static int mapSize, cnt = 0;
+    static ArrayList<Integer> store = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        //setUp
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        mapSize = Integer.parseInt(br.readLine());
+        for (int i = 0; i < mapSize; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < mapSize; i++) {
+            String[] inputLine = br.readLine().split("");
+            for (int j = 0; j < mapSize; j++) {
+                graph.get(i).add(Integer.parseInt(inputLine[j]));
+            }
+        }
+        visited = new boolean[mapSize][mapSize];
+        for (int i = 0; i < mapSize; i++) {
+            for (int j = 0; j < mapSize; j++) {
+                if (graph.get(i).get(j) == 1 && !visited[i][j]){
+                    bfs(i, j);
+                    store.add(cnt);
+                    cnt = 0;
+                }
+            }
+        }
+        outPut();
+    }
+
+    private static void outPut(){
+        System.out.println(store.size());
+        Collections.sort(store);
+        for (Integer answer : store) {
+            System.out.println(answer);
+        }
+    }
+
+    private static void bfs(int xVertex, int yVertex){
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {xVertex, yVertex});
+        visited[xVertex][yVertex] = true;
+        cnt++;
+        while (!queue.isEmpty()){
+            int getXVertex = queue.peek()[0];
+            int getYVertex = queue.peek()[1];
+            queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int newXVertex = getXVertex + addRow[i];
+                int newYVertex = getYVertex + addCol[i];
+                if (newXVertex >= 0 && newXVertex < mapSize && newYVertex >= 0 && newYVertex < mapSize){
+                    if (graph.get(newXVertex).get(newYVertex) == 1 && !visited[newXVertex][newYVertex]){
+                        queue.add(new int[]{newXVertex, newYVertex});
+                        visited[newXVertex][newYVertex] = true;
+                        cnt++;
+                    }
+                }
+            }
+        }
+    }
+
 }
