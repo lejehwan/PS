9e4d5c65ee7e47fada144418e137dc3e8984e732
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2023-01-26 10:41:21 +0900

    [Silver I] Title: 미로 탐색, Time: 188 ms, Memory: 16532 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[][] graph;
+    static boolean[][] visited;
+    static int row, col = 0;
+    static int[] addRow = {1, -1, 0, 0};
+    static int[] addCol = {0, 0, 1, -1};
+    static int[][] cost;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        row = Integer.parseInt(st.nextToken());
+        col = Integer.parseInt(st.nextToken());
+        graph = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            String[] inputLine = br.readLine().split("");
+            for (int j = 0; j < col; j++) {
+                graph[i][j] = Integer.parseInt(inputLine[j]);
+            }
+        }
+        visited = new boolean[row][col];
+        cost = new int[row][col];
+        bfs(0, 0);
+        outPut();
+    }
+
+    private static void outPut() {
+        System.out.println(graph[row - 1][col - 1]);
+    }
+
+    private static void bfs(int xVertex, int yVertex) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{xVertex, yVertex});
+        visited[xVertex][yVertex] = true;
+        cost[xVertex][yVertex] = 1;
+        while (!queue.isEmpty()) {
+            int getXVertex = queue.peek()[0];
+            int getYVertex = queue.peek()[1];
+            queue.poll();
+            for (int i = 0; i < 4; i++) {
+                int newXVertex = getXVertex + addRow[i];
+                int newYVertex = getYVertex + addCol[i];
+                if (newXVertex >= 0 && newXVertex < row && newYVertex >= 0 && newYVertex < col) {
+                    if (graph[newXVertex][newYVertex] == 1 && !visited[newXVertex][newYVertex]) {
+                        queue.add(new int[]{newXVertex, newYVertex});
+                        visited[newXVertex][newYVertex] = true;
+                        graph[newXVertex][newYVertex] = graph[getXVertex][getYVertex] + 1;
+                    }
+                }
+            }
+        }
+    }
+
+}
