817d4460080b26ca2fdc9cb33832f9871ac3d105
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2023-01-31 18:14:35 +0900

    [Gold V] Title: 토마토, Time: 664 ms, Memory: 126844 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -7,82 +7,82 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	private static int[][][] map;
-	private static int[] addRow = { 1, -1, 0, 0, 0, 0 };
-	private static int[] addCol = { 0, 0, 1, -1, 0, 0 };
-	private static int[] addFac = { 0, 0, 0, 0, 1, -1 };
-	private static int n, m, h;
-	private static int max = 0;
-
-	private static Queue<int[]> queue = new LinkedList<int[]>();
-
+	static int x, y, z, answer = 0;
+	static int[][][] graph;
+	static Queue<int[]> queue = new LinkedList<int[]>();
+	static int[] addZ = {-1, 1, 0, 0, 0, 0};
+	static int[] addX = {0, 0, -1, 1, 0, 0};
+	static int[] addY = {0, 0, 0, 0, -1, 1};
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer stz = new StringTokenizer(br.readLine());
-		m = Integer.parseInt(stz.nextToken());
-		n = Integer.parseInt(stz.nextToken());
-		h = Integer.parseInt(stz.nextToken());
-		map = new int[h][n][m];
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < n; j++) {
-				stz = new StringTokenizer(br.readLine());
-				for (int j2 = 0; j2 < m; j2++) {
-					int a = Integer.parseInt(stz.nextToken());
-					map[i][j][j2] = a;
-					if (a == 1) {
-						queue.add(new int[] { i, j, j2 });
-					}
-				}
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		x = Integer.parseInt(st.nextToken());
+		y = Integer.parseInt(st.nextToken());
+		z = Integer.parseInt(st.nextToken());
+		graph = new int[z][y][x];
+		int temp = 0;
+		for (int i = 0; i < y * z; i++) {
+			if(i != 0 && i % y == 0) temp ++;
+			String[] inputLine = br.readLine().split(" ");
+			for (int j = 0; j < x; j++) {
+				graph[temp][i % y][j] = Integer.parseInt(inputLine[j]);
+				if(graph[temp][i % y][j] == 1) queue.add(new int[] {temp, i % y, j});
 			}
 		}
-
-		bfs(queue);
-
-		if (!check()) {
-			System.out.println(-1);
-		} else if (max == 0) {
-			System.out.println(0);
-		} else {
-			System.out.println(max - 1);
-		}
+//		print();
+		bfs();
+		boolean check = checkTomato();
+		answer(check);
 	}
-
-	public static void bfs(Queue<int[]> queue) {
-
-		while (!queue.isEmpty()) {
-			int c = queue.peek()[0];
-			int a = queue.peek()[1];
-			int b = queue.peek()[2];
-			queue.poll();
-			for (int i = 0; i < 6; i++) {
-				int nz = c + addFac[i];
-				int nx = a + addRow[i];
-				int ny = b + addCol[i];
-
-				if (nx >= 0 && ny >= 0 && nz >= 0 && nx < n && ny < m && nz < h) {
-					if (map[nz][nx][ny] == 0) {
-						map[nz][nx][ny] = map[c][a][b] + 1;
-						queue.add(new int[] { nz, nx, ny });
-						if (map[nz][nx][ny] > max) {
-							max = map[nz][nx][ny];
-						}
-					}
-				}
-			}
-
-		}
+	
+	private static void answer(boolean check) {
+		if (!check) System.out.println(-1);
+		else if (answer == 0) System.out.println(0);
+		else System.out.println(answer - 1);
 	}
-
-	public static boolean check() {
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < n; j++) {
-				for (int j2 = 0; j2 < m; j2++) {
-					if (map[i][j][j2] == 0) {
-						return false;
-					}
-				}
+	
+	private static boolean checkTomato() {
+		int temp = 0;
+		for (int i = 0; i < y * z; i++) {
+			if(i != 0 && i % y == 0) temp ++;
+			for (int j = 0; j < x; j++) {
+				if(graph[temp][i % y][j] == 0) return false;
 			}
 		}
 		return true;
 	}
+	
+	private static void bfs() {
+		while (!queue.isEmpty()) {
+			int getZVertex = queue.peek()[0];
+			int getXVertex = queue.peek()[1];
+			int getYVertex = queue.peek()[2];
+			queue.poll();
+			for (int i = 0; i < addZ.length; i++) {
+				int newZVertex = getZVertex + addZ[i];
+				int newXVertex = getXVertex + addX[i];
+				int newYVertex = getYVertex + addY[i];
+				if (newZVertex >= 0 && newZVertex < z && newXVertex >= 0 && newXVertex < y && newYVertex >= 0 && newYVertex < x) {
+					if (graph[newZVertex][newXVertex][newYVertex] == 0) {
+						queue.add(new int[] {newZVertex, newXVertex, newYVertex});
+						graph[newZVertex][newXVertex][newYVertex] = graph[getZVertex][getXVertex][getYVertex] + 1;
+						answer = Math.max(answer, graph[newZVertex][newXVertex][newYVertex]);
+					}
+				}
+			}
+		}
+	}
+	
+	private static void print() {
+		for (int i = 0; i < z; i++) {
+			for (int j = 0; j < y; j++) {
+				for (int k = 0; k < x; k++) {
+					System.out.print(graph[i][j][k] + " ");
+				}
+				System.out.println();
+			}
+		}
+	}
+
 }
