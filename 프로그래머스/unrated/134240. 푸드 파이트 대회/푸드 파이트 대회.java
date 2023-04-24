4d180f087f95f07c74790fced85d482b42aa56f8
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2023-04-25 00:02:34 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 11.69 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,18 +1,14 @@
 class Solution {
     public String solution(int[] food) {
         String answer = "";
-        String temp = "";
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i < food.length; i++) {
             if (food[i] == 1) continue;
             for (int j = 0; j < food[i] / 2; j++) {
-                temp += i;
+                sb.append(i);
             }
         }
-        answer += temp + 0;
-        String[] str = temp.split("");
-        for (int i = str.length - 1; i >= 0; i--) {
-            answer += str[i];
-        }
+        answer += sb.toString() + 0 + sb.reverse();
         return answer;
     }
 }
