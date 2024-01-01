2f27b43ba2fc48e2628644a5f2934d5a0c391025
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2024-01-01 13:00:48 +0900

    [level unrated] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 1.30 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,51 +1,30 @@
 import java.util.*;
+// Ref #1
 class Solution {
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-        int[][] answer = {};
-        List<int[]> candidateSortList = categoryProcess(data, ext, val_ext);
-        sorting(candidateSortList, sort_by);
-        answer = convertListToArray(candidateSortList);
-        return answer;
-    }
-    
-    private static int[][] convertListToArray(List<int[]> candidateSortList) {
-        int [][]array = new int[candidateSortList.size()][4];
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                array[i][j] = candidateSortList.get(i)[j];
-            }
-        }
-        return array;
+        String[] arr = {"code", "date", "maximum", "remain"};
+        List<String> columnList = Arrays.asList(arr);
+        int extIdx = columnList.indexOf(ext);
+        int sortIdx = columnList.indexOf(sort_by);
+
+        List<int[]> candidateSortList = categoryProcess(data, val_ext, extIdx);
+        sorting(candidateSortList, sort_by, sortIdx);
+        return candidateSortList.toArray(int[][]::new);
     }
 
-    private static void sorting(List<int[]> idxList, String sort_by) {
+    private static void sorting(List<int[]> idxList, String sort_by, int sortIdx) {
         idxList.sort(new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                return switch (sort_by) {
-                    case "code" -> o1[0] - o2[0];
-                    case "date" -> o1[1] - o2[1];
-                    case "maximum" -> o1[2] - o2[2];
-                    case "remain" -> o1[3] - o2[3];
-                    default -> 0;
-                };
+                return o1[sortIdx] - o2[sortIdx];
             }
         });
     }
 
-    private static List<int[]> categoryProcess(int[][] data, String ext, int val_ext) {
-        int idx = -1;
-        switch (ext) {
-            case "code": idx = 0; break;
-            case "date": idx = 1; break;
-            case "maximum": idx = 2; break;
-            case "remain": idx = 3; break;
-            default: break;
-        }
-
+    private static List<int[]> categoryProcess(int[][] data, int val_ext, int extIdx) {
         List<int[]> candidateSortList = new ArrayList<>();
         for (int[] dataVal : data) {
-            if (dataVal[idx] < val_ext) candidateSortList.add(dataVal);
+            if (dataVal[extIdx] < val_ext) candidateSortList.add(dataVal);
         }
         return candidateSortList;
     }
