42b9be1c9d529ee7b40ce02a6a3028d290caf1fb
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2024-01-01 13:06:22 +0900

    [level unrated] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 5.48 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,5 +1,5 @@
 import java.util.*;
-// Ref #1
+// Ref #2
 class Solution {
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
         String[] arr = {"code", "date", "maximum", "remain"};
@@ -7,25 +7,10 @@ class Solution {
         int extIdx = columnList.indexOf(ext);
         int sortIdx = columnList.indexOf(sort_by);
 
-        List<int[]> candidateSortList = categoryProcess(data, val_ext, extIdx);
-        sorting(candidateSortList, sort_by, sortIdx);
-        return candidateSortList.toArray(int[][]::new);
+        return Arrays.stream(data)
+                .filter(eachData -> eachData[extIdx] < val_ext)
+                .sorted((o1, o2) -> o1[sortIdx] - o2[sortIdx])
+                .toArray(int[][]::new);
     }
 
-    private static void sorting(List<int[]> idxList, String sort_by, int sortIdx) {
-        idxList.sort(new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[sortIdx] - o2[sortIdx];
-            }
-        });
-    }
-
-    private static List<int[]> categoryProcess(int[][] data, int val_ext, int extIdx) {
-        List<int[]> candidateSortList = new ArrayList<>();
-        for (int[] dataVal : data) {
-            if (dataVal[extIdx] < val_ext) candidateSortList.add(dataVal);
-        }
-        return candidateSortList;
-    }
 }
