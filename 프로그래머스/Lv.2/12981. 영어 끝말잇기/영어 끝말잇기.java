cec6e5cff7c2d29a82dd8ce1b9ef04124695c2dc
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2024-01-12 08:50:47 +0900

    [level Lv.2] Title: 영어 끝말잇기, Time: 0.16 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/Lv.2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/Lv.2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,21 +1,16 @@
 import java.util.*;
 class Solution {
-    public int[] solution(int n, String[] words) {
-        Map<String, Integer> map = new HashMap<>();
-        map.put(words[0], 0);
-        int conCnt = 1;
+    public int[] solution(int n, String[] words) { 
+        Set<String> set = new HashSet<>();
+        set.add(words[0]);
         for (int i = 1; i < words.length; i++) {
-            if (i % n == 0) conCnt ++;
+            set.add(words[i]);
 
             char lastWord = words[i - 1].toCharArray()[words[i - 1].length() - 1];
             char firstWord = words[i].toCharArray()[0];
-            if (lastWord != firstWord || map.containsKey(words[i])) {
-                System.out.println(words[i]);
-                int num = (i + 1) % n == 0 ? n : (i + 1) % n;
-                return new int[] {num, conCnt};
+            if (lastWord != firstWord || set.size() != i + 1) {
+                return new int[] {i % n + 1, i / n + 1};
             }
-
-            map.put(words[i], 0);
         }
         return new int[] {0, 0};
     }
