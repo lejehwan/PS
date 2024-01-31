bc204b001e1d661bd693880ab07b9a713fefa78e
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2024-02-01 08:39:19 +0900

    [Silver III] Title: N과 M (4), Time: 144 ms, Memory: 16508 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+public class Main {
+    static int[] arr;
+    static int n = 0, m = 0;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
+        arr = new int[m];
+        sb = new StringBuilder();
+        dfs(0, 1);
+        System.out.println(sb.toString());
+    }
+
+    private static void dfs(int depth, int crt) {
+        if (depth == m) {
+            for (int val : arr) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = crt; i <= n; i++) {
+            arr[depth] = i;
+            dfs(depth + 1, i);
+        }
+    }
+
+}
