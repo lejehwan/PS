f40204692cc893aef670dd3347d4eaf80d951ae6
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2024-02-15 09:12:07 +0900

    [level 2] Title: 전화번호 목록, Time: 278.59 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,15 +1,10 @@
 import java.util.*;
 class Solution {
-    public boolean solution(String[] phone_book) {
-        Set<String> set = new HashSet<>();
-        for (String phone : phone_book) {
-            set.add(phone);
-        }
-        
-        for (String phone : phone_book) {
-            for (int i = 1; i < phone.length(); i++) {
-                String data = phone.substring(0, i);
-                if (set.contains(data)) return false;
+    public boolean solution(String[] phoneBook) {
+        Arrays.sort(phoneBook);
+        for(int i = 0; i < phoneBook.length-1; i++){
+            if(phoneBook[i+1].startsWith(phoneBook[i])){
+                return false;
             }
         }
         return true;
