aa5d15ef090d69cf90d3b797e51f26e43bdc140b
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2024-08-05 18:25:18 +0900

    [Silver II] Title: 유기농 배추, Time: 152 ms, Memory: 17428 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,64 +1,80 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class Main {
-    static int[][] graph;
-    static boolean[][] visited;
-    static int[] addRow = {0, 1, -1, 0};
-    static int[] addCol = {1, 0, 0, -1};
-    static int row, col = 0;
+
+    private static final int[] ADD_X = {1, -1, 0, 0};
+    private static final int[] ADD_Y = {0, 0, 1, -1};
+
+    private static int[][] maps;
+    private static boolean[][] visited;
+    private static int cnt = 0;
+    private static List<Integer> answerList = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
-        //setUp
+        solution();
+        printAnswer();
+    }
+
+    private static void printAnswer() {
+        answerList.forEach(System.out::println);
+    }
+
+    private static void exhaustiveSearch() {
+        for (int i = 0; i < maps.length; i++) {
+            for (int j = 0; j < maps[0].length; j++) {
+                if (maps[i][j] == 1 && !visited[i][j]) {
+                    bfs(i, j);
+                    cnt++;
+                }
+            }
+        }
+    }
+
+    private static void bfs(int x, int y) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[] {x, y});
+        visited[x][y] = true;
+
+        while (!queue.isEmpty()) {
+            int getX = queue.peek()[0];
+            int getY = queue.peek()[1];
+            queue.poll();
+            for (int i = 0; i < ADD_X.length; i++) {
+                int newX = getX + ADD_X[i];
+                int newY = getY + ADD_Y[i];
+                if (isValidMapSize(newX, newY) && maps[newX][newY] == 1 && !visited[newX][newY]) {
+                    queue.add(new int[] {newX, newY});
+                    visited[newX][newY] = true;
+                }
+            }
+        }
+    }
+
+    private static boolean isValidMapSize(int newX, int newY) {
+        return newX >= 0 && newX < maps.length && newY >= 0 && newY < maps[0].length;
+    }
+
+    private static void solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testCase = Integer.parseInt(br.readLine());
-        StringTokenizer st;
         for (int i = 0; i < testCase; i++) {
-            st = new StringTokenizer(br.readLine());
-            row = Integer.parseInt(st.nextToken());
-            col = Integer.parseInt(st.nextToken());
-            int cabbage = Integer.parseInt(st.nextToken());
-            graph = new int[col][row];
-            for (int j = 0; j < cabbage; j++) {
-                String[] inputLine = br.readLine().split(" ");
-                graph[Integer.parseInt(inputLine[1])][Integer.parseInt(inputLine[0])] = 1;
+            String[] line = br.readLine().split(" ");
+            maps = new int[Integer.parseInt(line[0])][Integer.parseInt(line[1])];
+            visited = new boolean[maps.length][maps[0].length];
+            for (int j = 0; j < Integer.parseInt(line[2]); j++) {
+                String[] input = br.readLine().split(" ");
+                maps[Integer.parseInt(input[0])][Integer.parseInt(input[1])] = 1;
             }
-            int cnt = 0;
-            visited = new boolean[col][row];
-            for (int j = 0; j < col; j++) {
-                for (int k = 0; k < row; k++) {
-                    if (graph[j][k] == 1 && !visited[j][k]){
-                        bfs(j,k);
-                        cnt++;
-                    }
-                }
-            }
-            System.out.println(cnt);
-        }
 
-    }
-
-    private static void bfs(int xVertex, int yVertex){
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[] {xVertex, yVertex});
-        visited[xVertex][yVertex] = true;
-        while (!queue.isEmpty()){
-            int getXVertex = queue.peek()[0];
-            int getYVertex = queue.peek()[1];
-            queue.poll();
-            for (int i = 0; i < addRow.length; i++) {
-                int newXVertex = getXVertex + addRow[i];
-                int newYVertex = getYVertex + addCol[i];
-                if (newXVertex >= 0 && newXVertex < col && newYVertex >=0 && newYVertex < row){
-                    if (graph[newXVertex][newYVertex] == 1 && !visited[newXVertex][newYVertex]){
-                        queue.add(new int[] {newXVertex, newYVertex});
-                        visited[newXVertex][newYVertex] = true;
-                    }
-                }
-            }
+            exhaustiveSearch();
+            answerList.add(cnt);
+            cnt = 0;
         }
     }
-
 }
