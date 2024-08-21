0a8f45f5e4991e13aa4c880cf9e5aca5023db411
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2024-08-21 23:10:10 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 100 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -3,51 +3,55 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[] visited = new int[101];
-    static int[] graph = new int[101];
-    static int[] nextStep = {1,2,3,4,5,6};
+    private static int[] map = new int[101];
+    private static boolean[] visited = new boolean[101];
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int ladder = Integer.parseInt(st.nextToken());
-        int snake = Integer.parseInt(st.nextToken());
-        for (int i = 1; i < graph.length; i++) {
-            graph[i] = i;
-        }
-        for (int i = 0; i < ladder + snake; i++) {
-            st = new StringTokenizer(br.readLine());
-            graph[Integer.parseInt(st.nextToken())] = Integer.parseInt(st.nextToken());
-        }
-        bfs(1);
-        print();
+        init();
+        bfs();
+        printAnswer();
     }
 
-    private static void print(){
-        System.out.println(visited[100]);
+    private static void printAnswer() {
+        System.out.println(map[map.length - 1]);
     }
 
-    private static void bfs(int startVertex){
+    private static void bfs() {
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(startVertex);
-        visited[startVertex] = 0;
-        while (!queue.isEmpty()){
-            int getVertex = queue.poll();
-            for (int i = 0; i < 6; i++) {
-                int newVertex = getVertex + nextStep[i];
-                if (newVertex > 1 && newVertex <= 100){
-                    if (visited[graph[newVertex]] == 0){
-                        queue.add(graph[newVertex]);
-                        visited[graph[newVertex]] = visited[getVertex] + 1;
+        queue.add(1);
+        visited[1] = true;
+
+        while (!queue.isEmpty()) {
+            int getX = queue.poll();
+            for (int i = 1; i <= 6; i++) {
+                int newX = getX + i;
+                if (isValidMapSize(newX)) {
+                    if (map[newX] != 0) newX = map[newX];
+                    if (!visited[newX]) {
+                        queue.add(newX);
+                        visited[newX] = true;
+                        map[newX] = map[getX] + 1;
                     }
-                    if (graph[newVertex] == 100) return;
                 }
             }
         }
     }
 
+    private static boolean isValidMapSize(int x) {
+        return x <= map.length - 1;
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int ladder = Integer.parseInt(input[0]);
+        int snake = Integer.parseInt(input[1]);
+        for (int i = 0; i < ladder + snake; i++) {
+            String[] data = br.readLine().split(" ");
+            map[Integer.parseInt(data[0])] = Integer.parseInt(data[1]);
+        }
+    }
 }
