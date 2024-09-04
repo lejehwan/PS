00642d50f850763082aeab51a902eee71fa0af32
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2024-09-05 07:27:12 +0900

    Update 유기농 배추.java

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,46 +1,43 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
-public class Main {
+class Main {
 
-    private static final int[] ADD_X = {1, -1, 0, 0};
-    private static final int[] ADD_Y = {0, 0, 1, -1};
+    static final int[] ADD_X = {1, -1, 0, 0};
+    static final int[] ADD_Y = {0, 0, 1, -1};
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[][] map;
+    static boolean[][] visited;
 
-    private static int[][] maps;
-    private static boolean[][] visited;
-    private static int cnt = 0;
-    private static List<Integer> answerList = new ArrayList<>();
-
-    public static void main(String[] args) throws IOException {
-        solution();
-        printAnswer();
+    public static void main(String[] args) throws Exception {
+        int testCaseCnt = readTestCaseCnt();
+        for (int i = 0; i < testCaseCnt; i++) {
+            init();
+            int answer = exhaustiveSearch();
+            System.out.println(answer);
+        }
+        closeStream();
     }
 
-    private static void printAnswer() {
-        answerList.forEach(System.out::println);
-    }
-
-    private static void exhaustiveSearch() {
-        for (int i = 0; i < maps.length; i++) {
-            for (int j = 0; j < maps[0].length; j++) {
-                if (maps[i][j] == 1 && !visited[i][j]) {
-                    bfs(i, j);
-                    cnt++;
+    private static int exhaustiveSearch() {
+        int answer = 0;
+        for (int j = 0; j < map.length; j++) {
+            for (int k = 0; k < map[0].length; k++) {
+                if (map[j][k] == 1 && !visited[j][k]) {
+                    bfs(j, k);
+                    answer ++;
                 }
             }
         }
+        return answer;
     }
 
     private static void bfs(int x, int y) {
         Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[] {x, y});
         visited[x][y] = true;
-
         while (!queue.isEmpty()) {
             int getX = queue.peek()[0];
             int getY = queue.peek()[1];
@@ -48,7 +45,7 @@ public class Main {
             for (int i = 0; i < ADD_X.length; i++) {
                 int newX = getX + ADD_X[i];
                 int newY = getY + ADD_Y[i];
-                if (isValidMapSize(newX, newY) && maps[newX][newY] == 1 && !visited[newX][newY]) {
+                if (isValidMapSize(newX, newY) && map[newX][newY] == 1 && !visited[newX][newY]) {
                     queue.add(new int[] {newX, newY});
                     visited[newX][newY] = true;
                 }
@@ -56,25 +53,26 @@ public class Main {
         }
     }
 
-    private static boolean isValidMapSize(int newX, int newY) {
-        return newX >= 0 && newX < maps.length && newY >= 0 && newY < maps[0].length;
+    private static boolean isValidMapSize(int x, int y) {
+        return x >= 0 && x < map.length && y >= 0 && y < map[0].length;
     }
 
-    private static void solution() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testCase = Integer.parseInt(br.readLine());
-        for (int i = 0; i < testCase; i++) {
-            String[] line = br.readLine().split(" ");
-            maps = new int[Integer.parseInt(line[0])][Integer.parseInt(line[1])];
-            visited = new boolean[maps.length][maps[0].length];
-            for (int j = 0; j < Integer.parseInt(line[2]); j++) {
-                String[] input = br.readLine().split(" ");
-                maps[Integer.parseInt(input[0])][Integer.parseInt(input[1])] = 1;
-            }
-
-            exhaustiveSearch();
-            answerList.add(cnt);
-            cnt = 0;
+    private static void init() throws Exception {
+        String[] input = br.readLine().split(" ");
+        map = new int[Integer.parseInt(input[0])][Integer.parseInt(input[1])];
+        visited = new boolean[map.length][map[0].length];
+        for (int i = 0; i < Integer.parseInt(input[2]); i++) {
+            String[] data = br.readLine().split(" ");
+            map[Integer.parseInt(data[0])][Integer.parseInt(data[1])] = 1;
         }
     }
+
+    private static int readTestCaseCnt() throws Exception {
+        return Integer.parseInt(br.readLine());
+    }
+
+    private static void closeStream() throws Exception {
+        br.close();
+    }
+
 }
