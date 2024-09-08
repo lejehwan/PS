e0251eacf08bb7268f65567f1b33e7d45c602eb1
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2024-09-09 07:38:52 +0900

    Update DFS와 BFS.java

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,79 +1,85 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class Main {
+class Main {
 
-    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static final String SPACE = " ";
+    static final String ENTER = "\n";
+    static int[][] map;
     static boolean[] visited;
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException {
-        //setUp
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int vertex = Integer.parseInt(st.nextToken());
-        int edge = Integer.parseInt(st.nextToken());
-        int startVertex = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < vertex + 1; i++) {
-            graph.add(new ArrayList());
-        }
-        for (int i = 0; i < edge; i++) {
-            st = new StringTokenizer(br.readLine());
-            int toVertex = Integer.parseInt(st.nextToken());
-            int fromVertex = Integer.parseInt(st.nextToken());
-            graph.get(toVertex).add(fromVertex);
-            graph.get(fromVertex).add(toVertex);
-        }
+    static int v;
+    static StringBuilder sb;
 
-        arraySort();
-
-        visited = new boolean[vertex + 1];
-        sb.append(startVertex).append(" ");
-        dfs(startVertex);
-
-        visited = new boolean[vertex + 1];
-        sb.append("\n").append(startVertex).append(" ");
-        bfs(startVertex);
-
-        outPut();
+    public static void main(String[] args) throws Exception {
+        init();
+        printDFS(v);
+        printBFS(v);
+        printAnswer();
     }
-    private static void outPut(){
+
+    private static void printAnswer() {
         System.out.println(sb.toString());
     }
 
-    private static void bfs(int vertex) {
-        visited[vertex] = true;
+    private static void printBFS(int v) {
+        visited = new boolean[map.length];
+        sb.append(ENTER);
+        sb.append(v).append(SPACE);
+        bfs(v);
+    }
+
+    private static void printDFS(int v) {
+        visited = new boolean[map.length];
+        sb.append(v).append(SPACE);
+        dfs(v);
+    }
+
+    private static void bfs(int v) {
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(vertex);
-        while (!queue.isEmpty()){
-            int getVertex = queue.poll();
-            for (int i = 0; i < graph.get(getVertex).size(); i++) {
-                int nextVertex = graph.get(getVertex).get(i);
-                if (!visited[nextVertex]){
-                    visited[nextVertex] = true;
-                    queue.add(nextVertex);
-                    sb.append(nextVertex).append(" ");
+        queue.add(v);
+        visited[v] = true;
+        while (!queue.isEmpty()) {
+            int getV = queue.poll();
+            for (int i = 1; i < map.length; i++) {
+                if (map[getV][i] != 0 && !visited[i]) {
+                    queue.add(i);
+                    visited[i] = true;
+                    sb.append(i).append(SPACE);
                 }
             }
         }
+
     }
 
-    private static void dfs(int vertex){
-        visited[vertex] = true;
-        for (int i = 0; i < graph.get(vertex).size(); i++) {
-            int getVertex = graph.get(vertex).get(i);
-            if (!visited[getVertex]){
-                visited[getVertex] = true;
-                sb.append(getVertex).append(" ");
-                dfs(getVertex);
+    private static void dfs(int v) {
+        visited[v] = true;
+        for (int i = 1; i < map.length; i++) {
+            if (map[v][i] != 0 && !visited[i]) {
+                sb.append(i).append(SPACE);
+                dfs(i);
             }
         }
     }
 
-    private static void arraySort(){
-        for (int i = 1; i < graph.size(); i++) {
-            Collections.sort(graph.get(i));
+
+    private static void init() throws IOException {
+        sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        map = new int[Integer.parseInt(input[0]) + 1][Integer.parseInt(input[0]) + 1];
+        v = Integer.parseInt(input[2]);
+        for (int i = 0; i < Integer.parseInt(input[1]); i++) {
+            String[] data = br.readLine().split(" ");
+            int startV = Integer.parseInt(data[0]);
+            int endV = Integer.parseInt(data[1]);
+            map[startV][endV] = 1;
+            map[endV][startV] = 1;
         }
+        br.close();
     }
+
+
 }
