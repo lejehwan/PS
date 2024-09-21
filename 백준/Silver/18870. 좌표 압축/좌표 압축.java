5f564e3a74e4bb989ae09be8c4f79d6ff22760c3
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2024-09-21 19:49:47 +0900

    [Silver II] Title: 좌표 압축, Time: 1852 ms, Memory: 291028 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,34 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
-// 리스트 2개로도 할 수는 있지만 비효율적 
-// 키와 값을 가지는 map 을 사용하므로써 효율적으로 풀이
 
-public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int[] plane = new int[n];
-		int[] temp = new int[n];
-		for (int i = 0; i < plane.length; i++) {
-			plane[i] = sc.nextInt();
-		}
-		temp = plane.clone();// 깊은 복사
-		Arrays.sort(temp);
-		Map<Integer, Integer> map = new HashMap<Integer, Integer>();// 좌표값, 인덱스
-		int index = 0;
-		for (int i : temp) {
-			if (!map.containsKey(i)) {// 이미 존재하는 값이 아니라면
-				map.put(i, index++);// 해당 키의 값(인덱스)을 증가
-			}
-		}
-		StringBuilder sb = new StringBuilder();
-		for (int i : plane) {
-			sb.append(map.get(i)).append(" ");// 좌표값을 통한 인덱스 가져오기	
-		}
-		System.out.println(sb.toString());
-		sc.close();
-	}
+class Main {
+
+    static int[] originData;
+    static int[] revisedData;
+    static Map<Integer, Integer> map = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        init();
+        String answer = solve();
+        System.out.println(answer);
+    }
+
+    private static String solve() {
+        Arrays.sort(revisedData);
+
+        int val = 0;
+        for (int data : revisedData) {
+            if (!map.containsKey(data)) {
+                map.put(data, val++);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int data : originData) {
+            sb.append(map.get(data)).append(" ");
+        }
+
+        return sb.toString().trim();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        originData = new int[N];
+        revisedData = new int[N];
+        String[] input = br.readLine().split(" ");
+        for (int i = 0; i < N; i++) {
+            int data = Integer.parseInt(input[i]);
+            originData[i] = revisedData[i] = data;
+        }
+        br.close();
+    }
+
+
 }
