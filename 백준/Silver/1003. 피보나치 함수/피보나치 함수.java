cc85c83c5d26e395366cc78071d719b8020e72b0
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2024-09-23 07:52:28 +0900

    [Silver III] Title: 피보나치 함수, Time: 120 ms, Memory: 15924 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -5,18 +5,42 @@ import java.io.InputStreamReader;
 class Main {
 
     static final int MAX = 40;
-    static int[][] dp;
+    static int[][] dp = new int[MAX + 1][2];
 
     public static void main(String[] args) throws IOException {
         int[] tc = init();
-        memoization();
+        topDown(tc);
+//        bottomUp(tc);
+    }
+
+    private static void bottomUp(int[] tc) {
+        dpBottomUp();
         for (int N : tc) {
-            printAnswer(N);
+            System.out.println(dp[N][0] + " " + dp[N][1]);
         }
     }
 
-    private static void memoization() {
-        dp = new int[MAX + 1][2];
+    private static void topDown(int[] tc) {
+        topDownInit();
+        for (int N : tc) {
+            dpTopDown(N);
+            System.out.println(dp[N][0] + " " + dp[N][1]);
+        }
+    }
+
+    private static void topDownInit() {
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                dp[i][j] = -1;
+            }
+        }
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+    }
+
+    private static void dpBottomUp() {
         dp[0][0] = 1;
         dp[1][1] = 1;
         for (int i = 2; i < dp.length; i++) {
@@ -25,10 +49,13 @@ class Main {
         }
     }
 
-    private static void printAnswer(int N) {
-        System.out.println(dp[N][0] + " " + dp[N][1]);
+    private static int[] dpTopDown(int N) {
+        if (dp[N][0] == -1 || dp[N][1] == -1) {
+            dp[N][0] = dpTopDown(N - 1)[0] + dpTopDown(N - 2)[0];
+            dp[N][1] = dpTopDown(N - 1)[1] + dpTopDown(N - 2)[1];
+        }
+        return dp[N];
     }
-
 
     private static int[] init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -38,7 +65,6 @@ class Main {
             tc[i] = Integer.parseInt(br.readLine());
         }
         br.close();
-
         return tc;
     }
 
