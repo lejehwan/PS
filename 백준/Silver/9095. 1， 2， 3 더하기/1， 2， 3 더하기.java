1bee0b1a4dce6ed4bcc1874634da236a0e578975
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2024-09-26 07:45:03 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 108 ms, Memory: 14440 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Main {
+
+    static int[] dp = new int[11];
+
+    public static void main(String[] args) throws IOException {
+        int[] tc = init();
+//        String answer = dpBottomUp(tc);
+        String answer = dpTopDown(tc);
+        System.out.println(answer);
+    }
+
+    private static String dpTopDown(int[] tc) {
+        return Arrays.stream(tc)
+                .mapToObj(t -> String.valueOf(topDown(t)))
+                .collect(Collectors.joining("\n"));
+    }
+
+    private static int topDown(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        if (n == 3) return 4;
+
+        dp[n] = topDown(n - 1) + topDown(n - 2) + topDown(n - 3);
+
+        return dp[n];
+    }
+
+    private static String dpBottomUp(int[] tc) {
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int i = 4; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        return Arrays.stream(tc)
+                .mapToObj(t -> String.valueOf(dp[t]))
+                .collect(Collectors.joining("\n"));
+    }
+
+    private static int[] init() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        int[] tc = new int[T];
+        for (int i = 0; i < T; i++) {
+            tc[i] = Integer.parseInt(br.readLine());
+        }
+        br.close();
+        return tc;
+    }
+
+
+}
