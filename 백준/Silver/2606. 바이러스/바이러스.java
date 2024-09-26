fb3e87059cb08f89554fd546777eb12f94233338
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2024-09-27 07:17:54 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,50 +1,71 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
-public class Main {
+class Main {
 
-    private static int[][] graph;
-    private static boolean[] visited;
-    private static int vertex, answer = 0;
+    static List<List<Integer>> graph = new ArrayList<>();
+    static boolean[] visited;
+    static int answer = 0;
 
     public static void main(String[] args) throws IOException {
         init();
-        dfs(1);
-        answer();
+        bfs(1);
+//        dfs(1);
+        System.out.println(answer);
     }
 
-    private static void dfs(int vertex) {
-        Stack<Integer> stack = new Stack<>();
-        visited[vertex] = true;
-        stack.push(vertex);
-        
-        while(!stack.isEmpty()) {
-            for (int i = 1; i < graph.length; i++) {
-                if (graph[vertex][i] != 0 && !visited[i]) {
+    private static void dfs(int startVertex) {
+        visited[startVertex] = true;
+        for (int i = 0; i < graph.get(startVertex).size(); i++) {
+            int newVertex = graph.get(startVertex).get(i);
+            if (!visited[newVertex]) {
+                visited[newVertex] = true;
+                answer ++;
+                dfs(newVertex);
+            }
+        }
+    }
+
+    private static void bfs(int startVertex) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(startVertex);
+        visited[startVertex] = true;
+
+        while (!queue.isEmpty()) {
+            int getVertex = queue.poll();
+            for (int i = 0; i < graph.get(getVertex).size(); i++) {
+                int newVertex = graph.get(getVertex).get(i);
+                if (!visited[newVertex]) {
+                    queue.add(newVertex);
+                    visited[newVertex] = true;
                     answer ++;
-                    dfs(i);
                 }
             }
-            stack.pop();
         }
     }
 
     private static void init() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        vertex = Integer.parseInt(br.readLine());
-        int edge = Integer.parseInt(br.readLine());
-        graph = new int[vertex + 1][vertex + 1];
-        visited = new boolean[vertex + 1];
-        for (int i = 0; i < edge; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int toVertex = Integer.parseInt(st.nextToken());
-            int fromVertex = Integer.parseInt(st.nextToken());
-            graph[toVertex][fromVertex] = 1;
-            graph[fromVertex][toVertex] = 1;
+        int computerCnt = Integer.parseInt(br.readLine());
+        visited = new boolean[computerCnt + 1];
+        for (int i = 0; i <computerCnt + 1; i++) {
+            graph.add(new ArrayList<>());
         }
+        int computerPairsCnt = Integer.parseInt(br.readLine());
+        for (int i = 0; i < computerPairsCnt; i++) {
+            String[] data = br.readLine().split(" ");
+            int start = Integer.parseInt(data[0]);
+            int end = Integer.parseInt(data[1]);
+            graph.get(start).add(end);
+            graph.get(end).add(start);
+        }
+        br.close();
     }
 
-    private static void answer() {
-        System.out.println(answer);
-    }
+
 }
