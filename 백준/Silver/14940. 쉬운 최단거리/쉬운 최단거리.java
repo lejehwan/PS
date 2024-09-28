298526253391e9c262465879ec1063ee0bd0f656
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2024-09-29 00:41:26 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 552 ms, Memory: 54044 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -19,13 +19,15 @@ class Main {
     }
 
     private static void printAnswer() {
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[0].length; j++) {
                 if (!visited[i][j] && map[i][j] == 1) map[i][j] = -1;
-                System.out.print(map[i][j] + " ");
+                sb.append(map[i][j]).append(" ");
             }
-            System.out.println();
+            sb.append("\n");
         }
+        System.out.println(sb);
     }
 
     private static void bfs() {
