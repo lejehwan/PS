d50db61567a9bd2ecdd9d4772e911c53547f74cd
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2024-09-30 07:41:43 +0900

    [Gold V] Title: 토마토, Time: 848 ms, Memory: 141400 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -3,75 +3,72 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    private static final int[] ADD_X = {1, -1, 0, 0};
-    private static final int[] ADD_Y = {0, 0, 1, -1};
-    private static int[][] map;
-    private static Queue<int[]> queue = new LinkedList<>();
-
+    static int row, col, answer = 0;
+    static int[][] graph;
+    static Queue<int[]> queue = new LinkedList<>();
+    static int[] addRow = {-1, 1, 0 ,0};
+    static int[] addCol = {0, 0, -1, 1};
     public static void main(String[] args) throws IOException {
-        init();
-        exhaustiveSearch();
-        printAnswer();
-    }
-
-    private static void printAnswer() {
-        int answer = 0;
-        for (int[] ints : map) {
-            for (int j = 0; j < map[0].length; j++) {
-                if (ints[j] == 0) {
-                    System.out.println(-1);
-                    return;
-                }
-                answer = Math.max(answer, ints[j]);
-            }
-        }
-        System.out.println(answer - 1);
-    }
-
-    private static void exhaustiveSearch() {
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[0].length; j++) {
-                if (map[i][j] == 1) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        row = Integer.parseInt(st.nextToken());
+        col = Integer.parseInt(st.nextToken());
+        graph = new int[col][row];
+        for (int i = 0; i < graph.length; i++) {
+            String[] inputLine = br.readLine().split(" ");
+            for (int j = 0; j < graph[i].length; j++) {
+                graph[i][j] = Integer.parseInt(inputLine[j]);
+                if (graph[i][j] == 1) {
                     queue.add(new int[] {i, j});
                 }
             }
         }
         bfs();
+        checkTomato();
+//        print();
     }
 
-    private static void bfs() {
-        while (!queue.isEmpty()) {
-            int getX = queue.peek()[0];
-            int getY = queue.peek()[1];
+    public static void checkTomato(){
+        for (int i = 0; i < graph.length; i++) {
+            for (int j = 0; j < graph[i].length; j++) {
+                if (graph[i][j] == 0) {
+                    System.out.println(-1);
+                    return;
+                }
+                answer = Math.max(answer, graph[i][j]);
+            }
+        }
+        System.out.println(answer - 1);
+    }
+
+    public static void bfs(){
+        while (!queue.isEmpty()){
+            int getXVertex = queue.peek()[0];
+            int getYVertex = queue.peek()[1];
             queue.poll();
-            for (int i = 0; i < ADD_X.length; i++) {
-                int newX = getX + ADD_X[i];
-                int newY = getY + ADD_Y[i];
-                if (isValidMapSize(newX, newY) && map[newX][newY] == 0) {
-                    queue.add(new int[] {newX, newY});
-                    map[newX][newY] = map[getX][getY] + 1;
+            for (int i = 0; i < addRow.length; i++) {
+                int newXVertex = getXVertex + addRow[i];
+                int newYVertex = getYVertex + addCol[i];
+                if (newXVertex >= 0 && newXVertex < col && newYVertex >= 0 && newYVertex < row){
+                    if (graph[newXVertex][newYVertex] == 0){
+                        queue.add(new int[] {newXVertex, newYVertex});
+                        graph[newXVertex][newYVertex] = graph[getXVertex][getYVertex] + 1;
+                    }
                 }
             }
         }
     }
 
-    private static boolean isValidMapSize(int x, int y) {
-        return x >= 0 && x < map.length && y >= 0 && y < map[0].length;
-    }
-
-
-    private static void init() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        map = new int[Integer.parseInt(input[1])][Integer.parseInt(input[0])];
-        for (int i = 0; i < map.length; i++) {
-            String[] line = br.readLine().split(" ");
-            for (int j = 0; j < map[0].length; j++) {
-                map[i][j] = Integer.parseInt(line[j]);
+    public static void print(){
+        for (int i = 0; i < graph.length; i++) {
+            for (int j = 0; j < graph[i].length; j++) {
+                System.out.print(graph[i][j] + " ");
             }
+            System.out.println();
         }
     }
 }
