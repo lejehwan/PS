3d896faf1a342e55e08ad555599650d72cdb9cab
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2024-10-09 18:29:53 +0900

    Update N과 M （3）.java
    [Silver III] Title: N과 M (3), Time: 448 ms, Memory: 68532 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,33 +1,51 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class Main {
-    static int[] arr;
-    static int n = 0, m = 0;
-    static StringBuilder sb;
+class Main {
+
+    static int N, M;
+    static int[] answer;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+    
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        n = Integer.parseInt(input[0]);
-        m = Integer.parseInt(input[1]);
-        arr = new int[m];
-        sb = new StringBuilder();
-        dfs(0);
-        System.out.println(sb.toString());
+        init();
+        backTracking(0);
+        System.out.println(sb);
     }
 
-    private static void dfs(int depth) {
-        if (depth == m) {
-            for (int val : arr) {
-                sb.append(val).append(" ");
-            }
-            sb.append("\n");
+    private static void backTracking(int depth) {
+        if (depth == M) {
+            answerPrint();
             return;
         }
 
-        for (int i = 0; i < n; i++) {
-            arr[depth] = i + 1;
-            dfs(depth + 1);
+        for (int i = 1; i <= N; i++) {
+            answer[depth] = i;
+            backTracking(depth + 1);
+        }
+    }
+
+    private static void answerPrint() {
+        sb.append(
+                IntStream.of(answer)
+                        .mapToObj(String::valueOf)
+                        .collect(Collectors.joining(" "))
+        ).append("\n");
+
+    }
+
+    private static void init() throws IOException {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String[] data = br.readLine().split(" ");
+            N = Integer.parseInt(data[0]);
+            M = Integer.parseInt(data[1]);
+            answer = new int[M];
+            visited = new boolean[N];
         }
     }
 
