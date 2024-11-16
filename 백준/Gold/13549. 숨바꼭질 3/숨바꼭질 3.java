64adc48eb7668c4e304cda4d01700b938a316b03
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2024-11-16 15:24:34 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 192 ms, Memory: 31572 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,13 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
 
 class Main {
 
     static int[] vertical = new int[100001];
     static boolean[] visited = new boolean[100001];
+
+    public static class Node implements Comparable<Node> {
+        int position;
+        int time;
+        public Node(int position, int time) {
+            this.position = position;
+            this.time = time;
+        }
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.time, o.time);
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
@@ -15,11 +27,12 @@ class Main {
             int N = Integer.parseInt(input[0]);
             int K = Integer.parseInt(input[1]);
 
-            System.out.println(bfs(N, K));
+//            System.out.println(dequeBfs(N, K));
+            System.out.println(priorityQueueBfs(N, K));
         }
     }
 
-    private static int bfs(int N, int K) {
+    private static int dequeBfs(int N, int K) {
         Deque<Integer> deque = new ArrayDeque<>();
         deque.add(N);
         while(!deque.isEmpty()) {
@@ -38,7 +51,30 @@ class Main {
                 }
             }
         }
-        return vertical[K];
+        return -1;
+    }
+
+    private static int priorityQueueBfs(int N, int K) {
+        Arrays.fill(vertical, Integer.MAX_VALUE);
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.add(new Node(N, 0));
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+            int now = node.position;
+            if (now == K) return node.time;
+            for (int[] move : movesWithWeight(now)) {
+                int next = move[0];
+                int weight = move[1];
+                if (isValid(next)) {
+                    int newCost = node.time + weight;
+                    if (newCost < vertical[next]) {
+                        vertical[next] = newCost;
+                        pq.add(new Node(next, newCost));
+                    }
+                }
+            }
+        }
+        return -1;
     }
 
     private static boolean isValid(int x) {
@@ -47,6 +83,14 @@ class Main {
 
     private static int[] moves(int x) {
         return new int[] {x - 1, x + 1, x * 2};
+    }
+
+    private static int[][] movesWithWeight(int x) {
+        return new int[][] {
+                {x - 1, 1},
+                {x + 1, 1},
+                {x * 2, 0}
+        };
     }
 
 }
